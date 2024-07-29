53f636ab2fcc499ec9c9ee67e2a94833ee83233e
amdnaseerch <106038235+amdnaseerch@users.noreply.github.com>
2024-07-29 13:02:15 +0200

    Update NodeCoordinator.java

--- hazelcast-assignment/src/main/java/com/assignment/hazelcast/NodeCoordinator.java
+++ hazelcast-assignment/src/main/java/com/assignment/hazelcast/NodeCoordinator.java
@@ -5,14 +5,11 @@ import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class NodeCoordinator {
 
 	private static final String LOCK_NODE = "/startupLock";
 	private ZooKeeperConnection zooKeeperConnection;
-	private static final Logger LOG = LoggerFactory.getLogger(NodeCoordinator.class);
 
 	public NodeCoordinator(ZooKeeperConnection zooKeeperConnection) {
 		this.zooKeeperConnection = zooKeeperConnection;
@@ -28,17 +25,8 @@ public class NodeCoordinator {
 				keeper.create(LOCK_NODE, "initialized".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
 				System.out.println("We are started!");
 			} catch (KeeperException e) {
-					throw e;
+				//No action	
 			}
-		} else {
-			// Node already exists, we need to check fi the startup has been initialized.
-			byte[] data = keeper.getData(LOCK_NODE, false, null);
-			if (!new String(data).equals("initialized")) {
-				// Updating node data to indicate intialization.
-				keeper.setData(LOCK_NODE, "initialized".getBytes(), -1);
-				System.out.println("We are started!");
-			}
-
 		}
 	}
 }
